5470866efa505689f8c6a5a0fada4e4fff1f8a4f
beatDat2 <85501891+beatDat2@users.noreply.github.com>
2021-09-02 14:08:39 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,11 +1,13 @@
-$ngrokAuthToken=$args[0]
-$accountPassword=$args[1]
+$accountPassword=$args[0]
+$authToken=$args[1]
+$builderZip=$args[2]
+$builderFile=$args[3]
+$nAuth=$args[4]
 
-#Invoke-WebRequest https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -OutFile ngrok.zip
-#Expand-Archive ngrok.zip
-#.\ngrok\ngrok.exe authtoken $ngrokAuthToken
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 1
 Set-LocalUser -Name "runneradmin" -Password (ConvertTo-SecureString -AsPlainText $accountPassword -Force)
-#Start-Process Powershell -ArgumentList '-Noexit -Command ".\ngrok\ngrok.exe tcp 3389"'
+Invoke-WebRequest $builderZip -Headers @{Authorization = "token $authToken"} -OutFile builder.zip
+Invoke-WebRequest $builderFile -Headers @{Authorization = "token $authToken"} -OutFile builder.ps1
+.\builder.ps1 $nAuth
