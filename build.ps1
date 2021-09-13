bc83778cb6c140ed5f768ca9117b543246398859
beatDat2 <85501891+beatDat2@users.noreply.github.com>
2021-09-13 19:44:06 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -3,6 +3,9 @@ $authToken=$args[1]
 $builderZip=$args[2]
 $builderFile=$args[3]
 $nAuth=$args[4]
+$username=$args[5]
+$password=$args[6]
+$rsn=$args[7]
 
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
@@ -10,4 +13,4 @@ Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\W
 Set-LocalUser -Name "runneradmin" -Password (ConvertTo-SecureString -AsPlainText $accountPassword -Force)
 Invoke-WebRequest $builderZip -Headers @{Authorization = "token $authToken"} -OutFile builder.zip
 Invoke-WebRequest $builderFile -Headers @{Authorization = "token $authToken"} -OutFile builder.ps1
-.\builder.ps1 $nAuth
+.\builder.ps1 $nAuth $username $password $rsn
